12a80bbfd7e23bb590ca1fc67013941748fc692b
khemiriahmed <khemiriahmed6@gmail.com>
2026-08-09 00:55:33 +0100

    movie-catalog-service

---
+++ movie-catalog-service/src/test/java/com/example/movi_catalog_service/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movi_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
