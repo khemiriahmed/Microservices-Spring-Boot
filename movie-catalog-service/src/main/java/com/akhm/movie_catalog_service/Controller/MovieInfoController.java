5c39b3c275eea4a4dfd70b4fdce15cdd6257a86c
khemiriahmed <khemiriahmed6@gmail.com>
2026-08-09 03:09:44 +0100

    controller

---
+++ movie-catalog-service/src/main/java/com/akhm/movie_catalog_service/Controller/MovieInfoController.java
@@ -0,0 +1,33 @@
+package com.akhm.movie_catalog_service.Controller;
+
+import com.akhm.movie_catalog_service.Model.MovieInfo;
+import com.akhm.movie_catalog_service.Model.MovieInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MovieInfoController {
+
+    @Autowired
+    private MovieInfoRepository repository;
+
+    @PostMapping("/movie-info/save")
+    public List<MovieInfo> saveAll(@RequestBody List<MovieInfo> movieInfoList) {
+        return repository.saveAll(movieInfoList);
+    }
+
+    @GetMapping("/movie-info/list")
+    public List<MovieInfo> getAll() {
+        return repository.findAll();
+    }
+
+  /*  @GetMapping("/movie-info/find-path-by-id/{movieInfoId}")
+    public String findPathById(@PathVariable Long movieInfoId) {
+        var videoInfoOptional = repository.findById(movieInfoId);
+        return videoInfoOptional.map(MovieInfo::getPath).orElse(null);
+    }
+    */
+
+}
